095309276145fd9ce21d97b7ffb2c10b3f7de3c4
Nadir Nadirli <companynadirnadirli@gmail.com>
2022-05-07 18:37:52 +0400

    I changed enter point.

--- src/Run.java
+++ src/Run.java
@@ -4,35 +4,7 @@ public class Run {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Cipher cipherCaller = new Cipher();
-        boolean proses = true;
-
-        while (proses) {
-            System.out.println(Menu.getMenu());
-            switch (scanner.nextInt()) {
-                case 1:
-                    System.out.println("Encrypting started");
-                    cipherCaller.cipher();
-                    System.out.println("Encrypting finished");
-                    break;
-                case 2:
-                    System.out.println("Decrypting started");
-                    cipherCaller.deCoder();
-                    System.out.println("Decrypting finished");
-                    break;
-                case 3:
-                    System.out.println("Analysing started");
-                    cipherCaller.bruteForce();
-                    break;
-                case 4:
-                    System.out.println("System closed");
-                    proses = false;
-                    break;
-                default:
-                    System.out.println("Choose correct function");
-                    break;
-            }
-        }
+        MenuUtil menuUtil = new MenuUtil();
+        menuUtil.Menu();
     }
 }
